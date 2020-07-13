2cde238ab4c7b76667c901f38b676bb93f61b788
Thousandhack <1391053697@qq.com>
2020-07-13 23:58:57 +0800

    upload ,上传图片到指定的目录下

--- gin_stu/gin_demo_05/main.go
+++ gin_stu/gin_demo_05/main.go
@@ -21,8 +21,8 @@ func main() {
 		// c.JSON(200, gin.H{"message": file.Header.Context})
 		// 传到项目目录，名字就用本身
 		//c.SaveUploadedFile(file, file.Filename)
-		// 传到自己指定的目录下，下面就会上传到src下gin_project/upload_file/ 的目录
-		c.SaveUploadedFile(file, "gin_stu/upload_file/"+file.Filename)
+		// 传到自己指定的目录下，下面就会上传到src下gin_stu/upload_file/ 的目录
+		c.SaveUploadedFile(file, "./upload_file/"+file.Filename)
 		//c.String(http.StatusOK, file.Filename)
 		c.JSON(http.StatusOK,gin.H{
 			"message": "save "+file.Filename,
