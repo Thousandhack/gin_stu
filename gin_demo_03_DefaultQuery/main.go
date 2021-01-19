9a5a1d016f58c413db37aedf4979da0f80855956
Thousandhack <1391053697@qq.com>
2021-01-19 23:25:31 +0800

    接收查询的参数对

--- gin_demo_03_DefaultQuery/main.go
+++ gin_demo_03_DefaultQuery/main.go
@@ -1,22 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+    "fmt"
+    "net/http"
 
-	"github.com/gin-gonic/gin"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/user", func(c *gin.Context) {
-		//指定默认值
-		//http://localhost:8000/user 才会打印出来默认的值 为小白
-		name := c.DefaultQuery("name", "小白")
-		c.String(http.StatusOK, fmt.Sprintf("hello %s", name))
-		// http://localhost:8000/user?name=hsz
-		// 返回结果为：
-		// hello hsz
-	})
-	r.Run(":8000")
+    r := gin.Default()
+    r.GET("/user", func(c *gin.Context) {
+        //指定默认值
+        //http://localhost:8000/user 才会打印出来默认的值 为:  hello 小白 age is 18
+        // http://localhost:8000/user?name=hsz 打印 hsz
+        // http://localhost:8000/user?name=hsz&age=22 打印： hello hsz age is 22
+        name := c.DefaultQuery("name", "小白")
+        age := c.DefaultQuery("age", "18")
+        addr := c.Query("addr")  // 这个如果addr没有相应的值，那么就是没有默认返回值
+        c.String(http.StatusOK, fmt.Sprintf("hello %s age is %s addr %s", name, age, addr))
+    })
+    r.Run(":8000")
 }
