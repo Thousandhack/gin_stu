df158444bc78bb228787ce08432daff592b04186
Thousandhack <1391053697@qq.com>
2021-01-20 23:10:07 +0800

    上传文件例子

--- gin_demo_05_uploadFile/main.go
+++ gin_demo_05_uploadFile/main.go
@@ -21,19 +21,12 @@ func main() {
 		// c.JSON(200, gin.H{"message": file.Header.Context})
 		// 传到项目目录，名字就用本身
 		//c.SaveUploadedFile(file, file.Filename)
-		// 传到自己指定的目录下，下面就会上传到src下gin_stu/upload_file/ 的目录
-		c.SaveUploadedFile(file, "./upload_file/"+file.Filename)
+		// 传到自己指定的目录下，下面就会上传到src下gin_project/upload_file/ 的目录
+		c.SaveUploadedFile(file, "upload_file/"+file.Filename)
 		//c.String(http.StatusOK, file.Filename)
 		c.JSON(http.StatusOK,gin.H{
-			"message": "save "+file.Filename,
+			"message": file.Filename,
 		})
 	})
 	r.Run()
 }
-
-/*
-	成功的返回结果：
-{
-    "message": "save black.jpg"
-}
-*/
