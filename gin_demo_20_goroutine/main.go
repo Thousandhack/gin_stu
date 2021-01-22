6aa0a38dc383e155d5acf280ed098d8c797415e2
Thousandhack <1391053697@qq.com>
2021-01-22 20:45:47 +0800

    gin框架goroutine异步的使用

---
+++ gin_demo_20_goroutine/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// 1.创建路由
+	// 默认使用了2个中间件Logger(), Recovery()
+	r := gin.Default()
+	// 1.异步
+	r.GET("/long_async", func(c *gin.Context) {
+		// 需要搞一个副本
+		copyContext := c.Copy()
+		// 异步处理
+		go func() {
+			time.Sleep(3 * time.Second)
+			log.Println("异步执行：" + copyContext.Request.URL.Path)
+		}()
+	})
+	// 2.同步
+	r.GET("/long_sync", func(c *gin.Context) {
+		time.Sleep(3 * time.Second)
+		log.Println("同步执行：" + c.Request.URL.Path)
+	})
+
+	r.Run(":8000")
+
+	/*
+		对同步和异步的url进行测试：
+	异步测试URL：http://127.0.0.1:8000/long_async
+	异步的url测试是及时返回对应的结果
+
+	同步测试URL:http://127.0.0.1:8000/long_sync
+	同步测试的结果是需要等待三秒再返回相应的结果
+	*/
+}
